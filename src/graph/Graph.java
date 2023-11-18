0ef4eac9af29c78f1f6f1c66c71dfcf935e16628
Mohad Naeem <mohad238695@gmail.com>
2023-11-19 00:56:12 +0500

    Return methods fixed for graph

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -75,10 +75,7 @@ public interface Graph<L> {
      * weights of those edges.
      * 
      * @param target a label
-     * @return a map where the key set is the set of labels of vertices such
-     *         that this graph includes an edge from that vertex to target, and
-     *         the value for each key is the (nonzero) weight of the edge from
-     *         the key to target
+     * @return 
      */
     public Map<L, Integer> sources(L target);
     
