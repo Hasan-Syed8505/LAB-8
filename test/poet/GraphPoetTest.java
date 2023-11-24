97985233bbc1a13ec6931337cb36e22949678a64
zaii-collab <zainbale73@gmail.com>
2023-11-24 21:52:44 +0500

    testGraphpoet generator and test constructor added

--- test/poet/GraphPoetTest.java
+++ test/poet/GraphPoetTest.java
@@ -21,5 +21,25 @@ public class GraphPoetTest {
     }
     
     // TODO tests
+    @Test
+    public void testGraphPoetConstructor() throws IOException {
+        // Test the GraphPoet(File) constructor with a sample corpus file
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+
+        // Add assertions to check the state of the GraphPoet object after
+        // construction
+        // For example, check that the graph is not empty
+        //assertTrue(!samplePoet.graph.toString().isEmpty());
+    }
+
+    @Test
+    public void testPoemGeneration1() throws IOException {
+        GraphPoet samplePoet = new GraphPoet(new File("test/poet/sample-corpus.txt"));
+        // Test the poem(String) method with different input strings
+
+        String input1 = "Test of the mugar system.";
+        String expectedOutput1 = "Test of of the the mugar mugar omni system.";
+        assertEquals(expectedOutput1, samplePoet.poem(input1));
+    }
     
 }
